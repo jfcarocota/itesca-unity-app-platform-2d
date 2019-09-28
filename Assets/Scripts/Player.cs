a9abdb4b7a399ef5d486dd1082528e9f5f6fb3fc
jfcarocota <jfcarocota@gmail.com>
2019-09-28 10:52:12 -0700

    physics added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,13 @@ public class Player : MonoBehaviour
     [SerializeField]
     float moveSpeed;
 
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +25,6 @@ public class Player : MonoBehaviour
     {
         Vector2 axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.Translate(Vector2.right * axis.x * moveSpeed * Time.deltaTime);
+        spriteRenderer.flipX = axis.x < 0 ? true : axis.x > 0 ? false : spriteRenderer.flipX; 
     }
 }
