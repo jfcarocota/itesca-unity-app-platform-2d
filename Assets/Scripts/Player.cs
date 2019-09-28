509d24ccdc972e6cc4d4cf9ea6ba349cc6334500
jfcarocota <jfcarocota@gmail.com>
2019-09-28 10:26:06 -0700

    movement x translate

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Translate(Vector2.right * axis.x * moveSpeed * Time.deltaTime);
+    }
+}
